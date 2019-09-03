eb2d1f23b05f9772e2f1ad8826d2102818d8e3ed
EC2 Default User <ec2-user@ip-172-31-36-196.ec2.internal>
2019-09-03 05:22:55 +0000

    test2

--- homework/labwork/lab1.php
+++ homework/labwork/lab1.php
@@ -2,28 +2,68 @@
 <html>
     <head>
         <title>Labwork 1: Hello World</title>
+        <style>
+        @import url('https://fonts.googleapis.com/css?family=Chilanka|Livvic&display=swap');
+        
+            body {
+                margin:10%;
+            }
+            
+            h1 {
+                font-family: 'Livvic', sans-serif;
+                color: darkblue;
+            }
+            
+            p {
+                font-family: 'Chilanka', cursive;
+                color: darkblue;
+            }
+            
+            a {
+            color: black;
+                
+            }
+            
+            
+        </style>
     </head>
     
     <body>
         <h1>About Me </h1>
         <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Hello! My name is Jordy and I pursuing a degree in Web Development. I have always been involved with technology ever since I was little, so I decided to pursue a degree in it.
+            However, there are so many fields you can specialize in. It took me several years to find out what I wanted to accomplish until I discovered Web Development. 
+            My first class was last summer at LBCC where I learned HTML/CSS. It was frustrating in the beginning because I couldnt always get the result that I was aiming for, but practice makes perfect.
+            So far, I still enjoy working on it and hope to learn lots about Web Development. Eventually, I will be good enough call myself a Full Stack Web Developer and start changing lives. 
+            As of recently, I created a <a href= https://github.com/jvjordy> Github Account</a> account. Feel free to check it out and let me know what you think about it.
             
-            <a href= https://github.com/jvjordy> My Github Account</a>
+            Other then working on my future, I like to:
+            <ul>
+                        <li>Watch movies and TV shows</li>
+                        <li>Spending time with friends and family</li>
+                        <li>Travel to mountains, cities, foreign countries, national parks, etc.</li>
+                    </ul>
         </p>
         
         <?php
+        
+        print "<p> 
+        My favorite websites are <a href= 'https://www.youtube.com/'>Youtube</a> and <a href= 'https://www.google.com/'> Google</a>. 
+        I usually get lost while watching either educational or entertainment videos. Google is mostly used for research purposes and to discover new fun things to do because coding can be a pain.
+        </p>";
+        
+        print "<p>
+        This assignment is not exactly hard, but it is a little bit confusing because there is a lot of creating and connecting accounts to each other. 
+        I got stuck a lot for many hours when I tried to make changes to my file and to update it afterwards to the other accounts. But it is actually easy, once you understand.
+        </p>";
+        
         /* 
         Name: Jordy
         Filename: lab1.php 
         Date: 09/01/2019
         
         */
-        echo 'This is my PHP Paragraph'
+        
         ?>
         
     </body>
