f2f4734d36a570d02f45b447a595a63e71bd49f0
EC2 Default User <ec2-user@ip-172-31-36-196.ec2.internal>
2019-09-10 08:07:58 +0000

    homework2

--- homework2/process_contact.php
+++ homework2/process_contact.php
@@ -6,6 +6,7 @@ $submit = $_POST ['contact.html'];
 $first_name = $_POST ['first_name'];
 $last_name = $_POST ['last_name'];
 $email = $_POST ['email'];
+d
 
 echo "<p> First Name: $first_name </p>";
     echo "<p> Last Name: $last_name </p>";
