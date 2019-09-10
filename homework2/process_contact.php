56732749869635c979b7c9d9c4ec8eff01e73b12
EC2 Default User <ec2-user@ip-172-31-36-196.ec2.internal>
2019-09-10 08:28:47 +0000

    homework2

--- homework2/process_contact.php
+++ homework2/process_contact.php
@@ -6,12 +6,17 @@ $submit = $_POST ['contact.html'];
 $first_name = $_POST ['first_name'];
 $last_name = $_POST ['last_name'];
 $email = $_POST ['email'];
-d
+$gender = $_POST ['gender'];
+
+echo '<h1> Thank you for supporting Steve Carrell.</h1>';
+echo '<h2> See your confirmation below.</h2>';
 
 echo "<p> First Name: $first_name </p>";
     echo "<p> Last Name: $last_name </p>";
 echo "<p> Email Address: $email</p>";
+echo "<p> Gender: $gender </p>";
 
+echo '<a href = "contact.html"> Return to form</a>';
 
 
 ?>
