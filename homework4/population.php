fe013c683e65e8492716335e405b69dac9a69714
EC2 Default User <ec2-user@ip-172-31-36-196.ec2.internal>
2019-09-27 00:01:38 +0000

    quote and population

--- homework4/population.php
+++ homework4/population.php
@@ -4,15 +4,60 @@
         <title>Population</title>
         <meta charset="utf-8">
         <style>
+        th {
+            text-align: center;
+        }
         
+    th, td {
+   padding: 15px;
+}
+
+    table, td {
+        border: 1px solid black;
+    }
         </style>
         </head>
         
         <body>
 
 <?php
+$populous = array("New York, New York"=>8622698, 
+    "Los Angeles, California"=>3999759, "Chicago, Illinois"=>2716450,
+    "Houston, Texas"=>2312717, "Phoenix, Arizona"=>1626078, 
+    "Philadelphia, Pennsylvania"=>1580863, "San Antonio, Texas"=>1511946,
+    "San Diego, California"=>1419516, "Dallas, Texas"=>1341075,
+    "San Jose, California"=>1035317, "Austin, Texas"=>950715, 
+    "Jacksonville, Florida"=>892062, "San Francisco"=>884363, 
+    "Columbus"=>879170, "Fort Worth"=>874168);
+   
+  $rank = "1";
+   
+   //numeric sort
+echo "<table><th>The 15 Most Populous Cities as of July 1, 2017</th>";
+echo "<tr><td>Rank</td>" . "<td>City, State</td>" . "<td>2017 Total Population</td></tr>";
 
-  echo "<p><a href='quote.html'><button>Return to form</button></a></p>";
+arsort($populous);
+foreach($populous as $location => $population) {
+    echo "<tr><td>" . $rank++ . "</td>" . "<td>" . $location . "</td>" ."<td>" . $population . "</td></tr>";
+}
+
+echo "</table><br><br>";
+   
+   $rank2 = "1";
+   
+   //alphabetical sort
+   echo "<table><th>The 15 Most Populous Cities as of July 1, 2017</th>";
+   echo "<tr><td>Rank</td>" . "<td>City, State</td>" . "<td>2017 Total Population</td></tr>";
+   
+ksort($populous);
+foreach($populous as $location => $population) {
+   echo "<tr><td>" . $rank2++ . "</td>" . "<td>" . $location . "</td>" ."<td>" . $population . "</td></tr>";
+}
+
+echo "</table>";
+
+
+  echo "<p><a href='quote.html'><button>Return to quote form</button></a></p>";
 
 ?>
 
