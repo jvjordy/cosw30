f39d39f9ffadbeb24bb24e10ccf02fa777b72439
jvjordy <jordy.vervuurt@gmail.com>
2019-11-13 07:57:41 +0000

    homework10

---
+++ homework10/database.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Create environment variables
+$hostname   = getenv('DB_HOSTNAME');
+$username   = getenv('DB_USERNAME_PUBLIC_PHP');
+$password   = getenv('DB_PW_PUBLIC_PHP');
+$dbname     = getenv('DB_NAME_PHP');
+
+// Establish the connection to the database
+$connection = mysqli_connect($hostname, $username, $password, $dbname);
+
+
+?>
